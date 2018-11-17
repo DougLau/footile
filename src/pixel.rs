fea58a76c72f4aca2ab1479055b886f4b49f3d2d
Doug Lau <doug.p.lau@gmail.com>
2018-11-16 19:58:33 -0600

    Added comments to pixel::Format trait.

--- src/pixel.rs
+++ src/pixel.rs
@@ -15,13 +15,15 @@ pub trait Format: Clone + Default {
     /// * `src` Source color.
     fn over(&mut [Self], &Mask, Self);
 
+    /// Divide out precomputed alpha.
     fn divide_alpha(&mut [Self]);
 
+    /// Get the PNG color type.
     fn color_type() -> ColorType {
         ColorType::RGBA
     }
 
-    /// Transmute a pixel slice into a u8 slice
+    /// Transmute a pixel slice into a u8 slice.
     fn as_u8_slice(pix: &[Self]) -> &[u8] {
         let len = pix.len() * mem::size_of::<Self>();
         let b = (pix as *const [Self]) as *const u8;
