71a7d3f382e58f3f19eb8fd09eca5df7cd917ff1
Doug Lau <doug.p.lau@gmail.com>
2018-11-17 09:50:19 -0600

    Added benchmarks for Gray8; renamed older benchmarks.

--- benches/fishyb.rs
+++ benches/fishyb.rs
@@ -5,47 +5,77 @@ extern crate footile;
 use footile::*;
 use criterion::Criterion;
 
-fn fill_fishy_256(c: &mut Criterion) {
-    c.bench_function("fill_fishy_256", |b| b.iter(|| fill_fishy(256)));
+fn fill_256(c: &mut Criterion) {
+    c.bench_function("fill_256", |b| b.iter(|| gray_fill(256)));
 }
 
-fn fill_fishy_512(c: &mut Criterion) {
-    c.bench_function("fill_fishy_512", |b| b.iter(|| fill_fishy(512)));
+fn fill_512(c: &mut Criterion) {
+    c.bench_function("fill_512", |b| b.iter(|| gray_fill(512)));
 }
 
-fn fill_fishy(i: u32) {
-    make_plotter(i).fill(&make_fishy(), FillRule::NonZero);
+fn gray_fill(i: u32) {
+    make_plotter_gray(i).fill(&make_fishy(), FillRule::NonZero);
 }
 
-fn compose_fishy_256(c: &mut Criterion) {
-    let mut p = make_plotter(256);
+fn stroke_256(c: &mut Criterion) {
+    c.bench_function("stroke_256", |b| b.iter(|| gray_stroke(256)));
+}
+
+fn stroke_512(c: &mut Criterion) {
+    c.bench_function("stroke_512", |b| b.iter(|| gray_stroke(512)));
+}
+
+fn gray_stroke(i: u32) {
+    make_plotter_gray(i).stroke(&make_fishy());
+}
+
+fn gray_over_256(c: &mut Criterion) {
+    let mut p = make_plotter_gray(256);
     p.fill(&make_fishy(), FillRule::NonZero);
-    c.bench_function("compose_fishy_256", move |b| {
+    c.bench_function("gray_over_256", move |b| {
+        let mut r = Raster::new(p.width(), p.height());
+        let m = p.mask();
+        b.iter(|| r.over(m, Gray8::new(100)))
+    });
+}
+
+fn gray_over_512(c: &mut Criterion) {
+    let mut p = make_plotter_gray(512);
+    p.fill(&make_fishy(), FillRule::NonZero);
+    c.bench_function("gray_over_512", move |b| {
+        let mut r = Raster::new(p.width(), p.height());
+        let m = p.mask();
+        b.iter(|| r.over(m, Gray8::new(100)))
+    });
+}
+
+fn rgba_over_256(c: &mut Criterion) {
+    let mut p = make_plotter_rgba(256);
+    p.fill(&make_fishy(), FillRule::NonZero);
+    c.bench_function("rgba_over_256", move |b| {
         let mut r = Raster::new(p.width(), p.height());
         let m = p.mask();
         b.iter(|| r.over(m, Rgba8::rgb(127, 96, 96)))
     });
 }
 
-fn compose_fishy_512(c: &mut Criterion) {
-    let mut p = make_plotter(512);
+fn rgba_over_512(c: &mut Criterion) {
+    let mut p = make_plotter_rgba(512);
     p.fill(&make_fishy(), FillRule::NonZero);
-    c.bench_function("compose_fishy_512", move |b| {
+    c.bench_function("rgba_over_512", move |b| {
         let mut r = Raster::new(p.width(), p.height());
         let m = p.mask();
         b.iter(|| r.over(m, Rgba8::rgb(127, 96, 96)))
     });
 }
 
-fn stroke_fishy(c: &mut Criterion) {
-    c.bench_function("stroke_fishy", |b| b.iter(|| stroke_fishy2()));
+fn make_plotter_gray(i: u32) -> Plotter<Gray8> {
+    let mut p = Plotter::new(i, i);
+    p.set_transform(Transform::new_scale(2f32, 2f32));
+    p
 }
 
-fn stroke_fishy2() {
-    make_plotter(256).stroke(&make_fishy());
-}
-
-fn make_plotter(i: u32) -> Plotter<Rgba8> {
+fn make_plotter_rgba(i: u32) -> Plotter<Rgba8> {
     let mut p = Plotter::new(i, i);
     p.set_transform(Transform::new_scale(2f32, 2f32));
     p
@@ -62,6 +92,6 @@ fn make_fishy() -> Path2D {
                 .build()
 }
 
-criterion_group!(benches, fill_fishy_256, fill_fishy_512, compose_fishy_256,
-    compose_fishy_512, stroke_fishy);
+criterion_group!(benches, fill_256, fill_512, stroke_256, stroke_512,
+    gray_over_256, gray_over_512, rgba_over_256, rgba_over_512);
 criterion_main!(benches);
