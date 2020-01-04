3cc7a282c957a8b04089adef303f91109771a1fa
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2020-01-04 15:54:39 -0600

    Get rid of call to removed function alpha_mode on RasterBuilder.

--- examples/fishy.rs
+++ examples/fishy.rs
@@ -1,6 +1,6 @@
 // fishy.rs
 use footile::{FillRule, PathBuilder, Plotter};
-use pix::{RasterBuilder, Rgba8, AlphaMode, Srgb, Associated, Ch8, Translucent, Rgb};
+use pix::{RasterBuilder, Rgba8, PremulRgba8};
 use pixops::raster_over;
 
 mod png;
@@ -25,20 +25,19 @@ fn main() -> Result<(), std::io::Error> {
         .line_to(-8.0, 8.0)
         .build();
     let mut p = Plotter::new(128, 128);
-    let mut r = RasterBuilder::<Rgb<Ch8, Translucent<Ch8>, Associated, Srgb>>::new()
-        .alpha_mode(AlphaMode::Associated)
+    let mut r = RasterBuilder::<PremulRgba8>::new()
         .with_clear(p.width(), p.height());
     raster_over(
         &mut r,
         p.fill(&fish, FillRule::NonZero),
-        Rgb::<Ch8, Translucent<Ch8>, Associated, Srgb>::new(127, 96, 96),
+        PremulRgba8::new(127, 96, 96),
         0,
         0,
     );
     p.clear_mask();
-    raster_over(&mut r, p.stroke(&fish), Rgb::<Ch8, Translucent<Ch8>, Associated, Srgb>::new(255, 208, 208), 0, 0);
+    raster_over(&mut r, p.stroke(&fish), PremulRgba8::new(255, 208, 208), 0, 0);
     p.clear_mask();
-    raster_over(&mut r, p.stroke(&eye), Rgb::<Ch8, Translucent<Ch8>, Associated, Srgb>::new(0, 0, 0), 0, 0);
+    raster_over(&mut r, p.stroke(&eye), PremulRgba8::new(0, 0, 0), 0, 0);
 
     let r = RasterBuilder::<Rgba8>::new()
         .with_raster(&r);
