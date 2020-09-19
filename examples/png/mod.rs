d2c3c481a808c965ce146d88250e4fba2ab4ceb9
Doug Lau <doug.p.lau@gmail.com>
2020-09-19 13:48:21 -0500

    Updated examples to png_pong 0.7

--- examples/png/mod.rs
+++ examples/png/mod.rs
@@ -4,18 +4,18 @@ use pix::el::Pixel;
 use pix::gray::SGray8;
 use pix::matte::Matte8;
 use pix::Raster;
-use png_pong::Format;
-use std::fs;
+use png_pong::Encoder;
+use std::fs::File;
 use std::io;
 
 /// Write a `Raster` to a file.
 pub fn write<P>(raster: &Raster<P>, filename: &str) -> io::Result<()>
 where
-    P: Pixel + Format,
+    P: Pixel,
 {
-    let mut out_data = vec![];
-    png_pong::FrameEncoder::new(&mut out_data).still(raster)?;
-    fs::write(filename, out_data)
+    let mut file = File::create(filename)?;
+    Encoder::new(&mut file).into_step_enc().still(raster);
+    Ok(())
 }
 
 /// Write a `Raster<Matte8>` to a grayscale file.
